1007ac3df11f4860abc981cb371139ac727d57ce
sandhya77 <ragulapadusandhya@gmail.com>
2017-04-13 21:15:11 +0530

    Update my_compare_test.go

--- my_compare_test.go
+++ my_compare_test.go
@@ -1,1 +1,40 @@
 
+package hacker
+
+
+import (
+
+	. "strings"
+
+	"testing"
+
+)
+
+
+var compareTests = []struct {
+
+	a, b string
+
+	i    int
+
+}{
+
+	{"", "", 0},
+
+	{"a", "", 1},
+
+	{"", "a", -1},
+
+	{"abc", "abc", 0},
+
+	{"ab", "abc", -1},
+
+	{"abc", "ab", 1},
+
+	{"x", "ab", 1},
+
+	{"ab", "x", -1},
+
+	{"x", "a", 1},
+
+	{"b", "x", -1},
